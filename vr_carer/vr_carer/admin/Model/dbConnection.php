10b0bd12e0c8a11b2edc292c29bf765dff5dbb91
Yunn Shwe Yi Win <yunnshweyiwin37@gmail.com>
2023-08-28 00:30:35 +0630

    update

--- vr_carer/vr_carer/admin/Model/dbConnection.php
+++ vr_carer/vr_carer/admin/Model/dbConnection.php
@@ -1,18 +1,13 @@
 <?php
+$hostname = "vr-carer-ysyw-do-user-14558665-0.b.db.ondigitalocean.com";
+$port = "25060";
+$dbname = "vr_carer";
+$username = "doadmin";
+$password = "AVNS_QXXt9axNKwe59BRIb-2";
+$pdo = new PDO(
+    "mysql:host=$hostname;port=$port;dbname=$dbname",
+    $username,
+    $password
+);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-try {
-    $hostname = "vr-carer-ysyw-do-user-14558665-0.b.db.ondigitalocean.com";
-    $port = "25060";
-    $dbname = "vr_carer";
-    $username = "doadmin";
-    $password = "AVNS_QXXt9axNKwe59BRIb-2";
-    $pdo = new PDO(
-        "mysql:host=$hostname;port=$port;dbname=$dbname",
-        $username,
-        $password
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "PDO Exception: " . $e->getMessage() . " (SQLSTATE " . $e->getCode() . ")";
-}
--- vr_carer/vr_carer/patient/Model/dbConnection.php
+++ vr_carer/vr_carer/patient/Model/dbConnection.php
@@ -1,18 +1,11 @@
 <?php
+$hostname = "vr-carer-ysyw-do-user-14558665-0.b.db.ondigitalocean.com";
+$port = "25060";
+$dbname = "vr_carer";
+$username = "doadmin";
+$password = "AVNS_QXXt9axNKwe59BRIb-2";
+$pdo = new PDO(
+    "mysql:host=$hostname;port=$port;dbname=$dbname", 
+$username, $password);
 
 
-
-try {
-    $hostname = "vr-carer-ysyw-do-user-14558665-0.b.db.ondigitalocean.com";
-    $port = "25060";
-    $dbname = "vr_carer";
-    $username = "doadmin";
-    $password = "AVNS_QXXt9axNKwe59BRIb-2";
-    $pdo = new PDO(
-            "mysql:host=$hostname;port=$port;dbname=$dbname",
-            $username,
-            $password
-        );
-} catch (PDOException $e) {
-    echo "PDO Exception: " . $e->getMessage() . " (SQLSTATE " . $e->getCode() . ")";
-}
