14cbdc39c2fb6bdf276f755a62dadade65e67a6f
Yunn Shwe Yi Win <yunnshweyiwin37@gmail.com>
2023-08-27 23:48:43 +0630

    .

--- vr_carer/vr_carer/admin/Model/dbConnection.php
+++ vr_carer/vr_carer/admin/Model/dbConnection.php
@@ -1,12 +1,13 @@
 <?php
-$hostname = "localhost";
-$port = "3306";
+$hostname = "vr-carer-ysyw-do-user-14558665-0.b.db.ondigitalocean.com";
+$port = "25060";
 $dbname = "vr_carer";
-$username = "root";
-$password = "";
+$username = "doadmin";
+$password = "AVNS_QXXt9axNKwe59BRIb-2";
 $pdo = new PDO(
     "mysql:host=$hostname;port=$port;dbname=$dbname",
     $username,
     $password
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
--- vr_carer/vr_carer/patient/Model/dbConnection.php
+++ vr_carer/vr_carer/patient/Model/dbConnection.php
@@ -1,9 +1,9 @@
 <?php
-$hostname = "localhost";
-$port = "3306";
+$hostname = "vr-carer-ysyw-do-user-14558665-0.b.db.ondigitalocean.com";
+$port = "25060";
 $dbname = "vr_carer";
-$username = "root";
-$password = "";
+$username = "doadmin";
+$password = "AVNS_QXXt9axNKwe59BRIb-2";
 $pdo = new PDO(
     "mysql:host=$hostname;port=$port;dbname=$dbname", 
 $username, $password);
