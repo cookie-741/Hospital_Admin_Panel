4abc9771e2cb21310ce5ffd3141af9660601635b
Yunn Shwe Yi Win <yunnshweyiwin37@gmail.com>
2023-08-27 23:55:47 +0630

    ..

--- vr_carer/vr_carer/admin/Model/dbConnection.php
+++ vr_carer/vr_carer/admin/Model/dbConnection.php
@@ -1,7 +1,7 @@
 <?php
 $hostname = "vr-carer-ysyw-do-user-14558665-0.b.db.ondigitalocean.com";
 $port = "25060";
-$dbname = "defaultdb";
+$dbname = "vr_carer";
 $username = "doadmin";
 $password = "AVNS_QXXt9axNKwe59BRIb-2";
 $pdo = new PDO(
